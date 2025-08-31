d35f4f221ca1f58ef3fded93cfdec796ab4fd373
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-08-31 12:23:01 -0300

    Create testcase.lua

---
+++ testcase.lua
@@ -0,0 +1,12 @@
+--loading the framework
+local describe = require('unit_to_the_moon')
+
+--runnint set of tests
+describe('Test function', function ()
+  it('your test name here', function ()  
+    local expected_name = "I am a string"
+    local obtained_name = "I am a string" --do some action that results the expected string
+    local failure_comment = "Expected name was "..expected_name..", but is "..obtained_name
+    assert(obtained_name == expected_name, failure_comment)
+  end)
+end)
