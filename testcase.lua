c588bc1b7f7ec41cd3c11681f041a39f2e49b7a8
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-08-31 12:33:36 -0300

    Update testcase.lua

--- testcase.lua
+++ testcase.lua
@@ -3,9 +3,15 @@ local describe = require('lunatic')
 
 --runnint set of tests
 describe('Test function', function ()
-  it('your test name here', function ()  
+  it('This is a passing test', function ()  
     local expected_name = "I am a string"
     local obtained_name = "I am a string" --do some action that results the expected string
+    local failure_comment = "Expected name was "..expected_name..", but is "..obtained_name
+    assert(obtained_name == expected_name, failure_comment)
+  end)
+  it('This is a failing test', function ()  
+    local expected_name = "I am a string"
+    local obtained_name = "I am a number" --do some action that results the expected string
     local failure_comment = "Expected name was "..expected_name..", but is "..obtained_name
     assert(obtained_name == expected_name, failure_comment)
   end)
